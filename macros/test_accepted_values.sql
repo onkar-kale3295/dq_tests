05ab0b71e3d52fda82e49a989d5cf9e6b9145958
onkarkale3295 <onkar_kale@persistent.com>
2021-04-21 15:09:52 +0530

    Macro changes

--- macros/test_accepted_values.sql
+++ macros/test_accepted_values.sql
@@ -2,18 +2,11 @@
 
 {% set quote_values = kwargs.get('quote', True) %}
 
-with all_values as (
-    select distinct
-        {{ column_name }} as value_field
-
-    from {{ model }}
-),
-
-validation_errors as (
+with validation_errors as (
     select
-        value_field
-    from all_values
-    where value_field not in (
+        {{ column_name }}
+    from {{ model }}
+    where {{ column_name }} not in (
         {% for value in values -%}
             {% if quote_values -%}
             '{{ value }}'
