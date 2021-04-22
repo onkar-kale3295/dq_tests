8816cb1ef437b58623fe308ad08d5daf4568e188
onkarkale3295 <onkar_kale@persistent.com>
2021-04-22 14:30:36 +0530

    Macro updates

--- macros/test_accepted_values.sql
+++ macros/test_accepted_values.sql
@@ -1,20 +1,26 @@
-{% macro test_accepted_values(model,column_name,values,dq_dimension = 'Validity',rule_description = 'To check if there data has any garbage values or unacceptable values',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40) %}
+{% macro test_accepted_values(model,column_name,values,sql,dq_dimension = 'Validity',rule_description = 'To check if there data has any garbage values or unacceptable values',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40) %}
 
 {% set quote_values = kwargs.get('quote', True) %}
+
+{% if values %}
+    set txt = {% for value in values -%}
+                    {% if quote_values -%}
+                    '{{ value }}'
+                    {%- else -%}
+                    {{ value }}
+                    {%- endif -%}
+                    {%- if not loop.last -%},{%- endif %}
+              {%- endfor %}
+{% elif sql %}
+    set txt = sql
+{% endif %}
 
 with validation_errors as (
     select
         {{ column_name }}
     from {{ model }}
     where {{ column_name }} not in (
-        {% for value in values -%}
-            {% if quote_values -%}
-            '{{ value }}'
-            {%- else -%}
-            {{ value }}
-            {%- endif -%}
-            {%- if not loop.last -%},{%- endif %}
-        {%- endfor %}
+        {{ txt }}
     )
 )
 
---
+++ macros/test_referential_integrity.sql
@@ -0,0 +1,27 @@
+{% macro test_referential_integrity(model,to,field,dq_dimension = 'Consistency',rule_description = 'To check if PK-FK relationship is correct',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40) %}
+
+{% set column_name = kwargs.get('column_name', kwargs.get('from')) %}
+
+select 
+    count(*) as validation_errors,
+    '{{ dq_dimension }}' as dq_dimension,
+    '{{ rule_description }}' as rule_description,
+    '{{ quality_threshold_lowerbound }}' as quality_threshold_lowerbound,
+    '{{ quality_threshold_upperbound }}' as quality_threshold_upperbound
+from (
+    select {{ column_name }} as id from {{ model }}
+) as child
+left join (
+    select {{ field }} as id from {{ to }}
+) as parent on parent.id = child.id
+where child.id is not null
+  and parent.id is null
+
+{% endmacro %}
+
+
+
+{% test relationships(model, to, field) %}
+    {% set macro = adapter.dispatch('test_referential_integrity') %}
+    {{ macro(model, to, field, **kwargs) }}
+{% endtest %}
