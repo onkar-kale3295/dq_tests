d096311282153f101393e813e3f29696e38b6d71
onkarkale3295 <onkar_kale@persistent.com>
2021-04-26 12:15:06 +0530

    Macro updates

--- macros/test_max.sql
+++ macros/test_max.sql
@@ -1,11 +1,14 @@
 {% macro test_max(model,column_name,value,operator = '=',dq_dimension = 'Validity',rule_description = 'To check the max column value',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40) %}
 
 select 
-    max_value as max_value,
-    flag as result
-from 
-    (select max({{ column_name }}) as max_value, case when max({{ column_name }}) {{ operator }} {{ value }} then 'PASS'
-     else 'FAIL' end as flag from {{ model }})
-where flag = 'FAIL'
-
+    count(*) as validation_errors from
+    (
+        select 
+            max_value as max_value,
+            flag as result
+        from 
+            (select max({{ column_name }}) as max_value, case when max({{ column_name }}) {{ operator }} {{ value }} then 'PASS'
+            else 'FAIL' end as flag from {{ model }})
+        where flag = 'FAIL'
+    )
 {% endmacro %}
--- macros/test_min.sql
+++ macros/test_min.sql
@@ -1,11 +1,14 @@
 {% macro test_min(model,column_name,value,operator = '=',dq_dimension = 'Validity',rule_description = 'To check the min column value',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40) %}
 
 select 
-    min_value as min_value,
-    flag as result
-from 
-    (select min({{ column_name }}) as min_value, case when min({{ column_name }}) {{ operator }} {{ value }} then 'PASS'
-     else 'FAIL' end as flag from {{ model }})
-where flag = 'FAIL'
-
+    count(*) as validation_errors from
+    (
+        select 
+            min_value as min_value,
+            flag as result
+        from 
+            (select min({{ column_name }}) as min_value, case when min({{ column_name }}) {{ operator }} {{ value }} then 'PASS'
+            else 'FAIL' end as flag from {{ model }})
+        where flag = 'FAIL'
+    )
 {% endmacro %}
