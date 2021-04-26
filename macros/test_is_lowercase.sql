9a3d2e4b78d116d76becf112356aaf70fbcc87c0
onkarkale3295 <onkar_kale@persistent.com>
2021-04-26 12:35:29 +0530

    Macro updates

---
+++ macros/test_is_lowercase.sql
@@ -0,0 +1,11 @@
+{% macro test_is_lowercase(model,column_name,dq_dimension = 'Accuracy',rule_description = 'To check tif the column values are in Lower Case',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40)%}
+
+select 
+    count(*) as validation_errors
+from (
+        select *,{{ column_name }} regexp '[a-z0-9]' as matches
+            from  {{ model }} 
+            where matches = 'FALSE'
+    )
+
+{% endmacro %}
---
+++ macros/test_is_uppercase.sql
@@ -0,0 +1,11 @@
+{% macro test_is_uppercase(model,column_name,dq_dimension = 'Accuracy',rule_description = 'To check tif the column values are in Upper Case',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40)%}
+
+select 
+    count(*) as validation_errors
+from (
+        select *,{{ column_name }} regexp '[A-Z0-9]' as matches
+            from  {{ model }} 
+            where matches = 'FALSE'
+    )
+
+{% endmacro %}
