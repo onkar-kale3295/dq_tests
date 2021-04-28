182e806922e6a97b47488a081b96d1b8f63eb28c
onkarkale3295 <onkar_kale@persistent.com>
2021-04-28 11:36:38 +0530

    Macro Update

--- macros/test_match_regex.sql
+++ macros/test_match_regex.sql
@@ -1,4 +1,4 @@
-{% macro test_match_regex(model,column_name,expression,dq_dimension = 'Validity',rule_description = 'To check if the data values match a text or regex pattern',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40,)%}
+{% macro test_match_regex(model,column_name,expression,dq_dimension = 'Validity',rule_description = 'To check if the data values match a text or regex pattern',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40)%}
 
 select 
     count(*) as validation_errors
