bd703e9da663840e9f7b7d5301e7d758f03ba211
onkarkale3295 <onkar_kale@persistent.com>
2021-06-04 17:52:06 +0530

    Added ML tests

---
+++ macros/test_outlier_multiple_column.sql
@@ -0,0 +1,11 @@
+{% macro test_outlier_multiple_column (model,column_name,upper_limit,lower_limit,dq_dimension = 'Validity',rule_description = 'To check if the data values are within fixed range. The range can be defined by the user for a specific datatype',quality_threshold_upperbound = 80,quality_threshold_lowerbound = 40) %}
+
+select 
+    count(*) as validation_errors
+from (
+    select * 
+    from {{ model }}
+    where {{ column_name }} NOT BETWEEN {{ lower_limit }} AND {{ upper_limit }}
+    )
+
+{% endmacro %}
